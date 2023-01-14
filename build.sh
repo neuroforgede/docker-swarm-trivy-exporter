ab09dbe9e6e8de3aaf4d6103ce541a8fce7acc3d
martinb <martinbraun123@aol.com>
2023-01-14 11:36:49 +0100

    move to github actions

--- build.sh
+++
@@ -1,8 +0,0 @@
- #!/bin/bash
- docker build -f Dockerfile \
-    -t neuroforgede/docker-swarm-trivy-exporter:latest \
-    -t neuroforgede/docker-swarm-trivy-exporter:0.1 \
-    .
-
-docker push neuroforgede/docker-swarm-trivy-exporter:latest
-docker push neuroforgede/docker-swarm-trivy-exporter:0.1
